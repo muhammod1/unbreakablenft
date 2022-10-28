32b69efbb98d720aa30e28313d2a1590a3511232
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-10-28 10:33:52 +0100

    [CHORE]-error-boundary

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import { DonateModal, Footer, Navbar } from "./components";
 
 import { About, Home, Nfts, Partnership, Program, Roadmap } from "./pages";
 import NotFound from "./pages/NotFound";
+import { ErrorBoundary, useErrorHandler } from 'react-error-boundary';
+
+function ErrorFallback({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: 'red' }}>{error.message}</pre>
+    </div>
+  );
+}
 
 const App = () => {
   return (
     <BrowserRouter>
       <div>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
         <DonateModal />
         <Navbar />
         <Routes>
@@ -20,6 +31,7 @@ const App = () => {
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
+        </ErrorBoundary>
       </div>
     </BrowserRouter>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as DonateModal } from "./DonateModal";
 export { default as Footer } from "./Footer";
+
