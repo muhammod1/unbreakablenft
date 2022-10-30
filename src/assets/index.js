c40be20b80dafa57fc085ae25f069eda7d71ae30
kcpele <fidekg123@gmail.com>
2022-10-30 23:30:00 +0100

    done with events

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,17 +6,22 @@ import board from "./board.png";
 import nftrobot from "./nftrobot.png";
 import ArrowIcon from "./ArrowIcon.svg";
 import activeJourney from "./activejourney.png";
-import Union from "./Union.svg"
-import instag from "./instag.png"
-import twit from "./twit.png"
-import faceb from "./faceb.png"
-import linkd from "./linkd.png"
-import event1 from "./event1.png"
-import event2 from "./event2.png"
-import event3 from "./event3.png"
-import host from "./host.png"
-import frame from "./frame.png"
-
+import Union from "./Union.svg";
+import instag from "./instag.png";
+import twit from "./twit.png";
+import faceb from "./faceb.png";
+import linkd from "./linkd.png";
+import event1 from "./event1.png";
+import event2 from "./event2.png";
+import event3 from "./event3.png";
+import host from "./host.png";
+import frame from "./frame.png";
+import eventscard from "./eventscard.png";
+import eventimage from "./eventimage.png";
+import eventimage1 from "./eventimage1.png";
+import eventimage2 from "./eventimage2.png";
+import eventimage3 from "./eventimage3.png";
+import eventimage4 from "./eventimage4.png";
 import schoolBatch from "./schoolbatch.png";
 import schoolImage from "./schoolimage.png";
 import company1 from "./company1.png";
@@ -41,8 +46,20 @@ export {
   company5,
   event1,
   nftrobot,
- ArrowIcon,
- Union, instag, linkd, twit, faceb,  event2, event3, host, frame
-    
+  ArrowIcon,
+  Union,
+  instag,
+  linkd,
+  twit,
+  faceb,
+  event2,
+  event3,
+  host,
+  frame,
+  eventimage,
+  eventimage1,
+  eventimage2,
+  eventimage3,
+  eventimage4,
+  eventscard,
 };
-
