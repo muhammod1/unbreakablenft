0865438385334cb4ffcbed281f39262901953b08
muhammod1 <muhammodholawale@gmail.com>
2022-11-01 08:24:39 +0100

    .

--- src/components/program/ProgramCard.js
+++ src/components/program/ProgramCard.js
@@ -9,18 +9,18 @@ const ProgramCard = ({ schoolDetails }) => {
   
   return (
     <div className='bg-[#0C0B09] pt-[30px]'>
-      <div className='px-[5%]'>
+      <div className='px-[5%] '>
         {schoolDetails.map(data => (
           <div key={data.id} className='max-w-5xl md:mx-auto border-b border-[#171717]'>
-            <div  className='py-[40px] flex flex-col md:gap-5 md:flex-row justify-center w-full'>
+            <div  className='w-full small_l:mx-auto mobile:w-[350px] md:w-full  py-[40px] flex flex-col md:gap-5 md:flex-row justify-center'>
               <div className='my-auto md:w-[10%]'>
                 <img src={data.number} alt="number" />
                 <p className='text-[#737373] my-2'>{data.institute}</p>
               </div>
               <div className='md:w-[80%]  my-5  flex flex-col md:flex-row gap-4 justify-between'>
-                <div className='w-full h-[260px] md:w-[40%] rounded-lg relative overflow-hidden' >
+                <div className='h-[240px] md:w-[40%] rounded-lg relative overflow-hidden' >
                   <img className='w-full h-full' src={data.bgImage} alt="number" />
-                  <div className='absolute  h-[80%] w-fit flex flex-col justify-between top-[7.5%] left-[10%]'>
+                  <div className='absolute h-[80%] w-fit flex flex-col justify-between top-[7.5%] left-[10%]'>
                     <img className='w-fit pl-5' src={data.logo} alt="number" />
                     <div>
                       <p className='text-white font-bold text-[24px]'>{data.imgTitle}</p>
@@ -28,10 +28,10 @@ const ProgramCard = ({ schoolDetails }) => {
                     </div>
                   </div>
                 </div>
-                <div className='my-4 md:w-[65%]'>
+                <div className='my-4 md:w-[60%]'>
                   <p className='text-yellow text-[14px] '>{data.title}</p>
                   <div className='text-white text-[12px] mt-2'>{data.desc}</div>
-                  <div className='text-[#E5E5E5] mt-5 text-[11px] flex md:justify-start md:gap-5 justify-between'>
+                  <div className='text-[#E5E5E5] mt-5 text-[11px] flex justify-start gap-3'>
                     <span className='bg-[#171717] p-2 rounded-full' >{data.prefrence.title1}</span>
                     <span className='bg-[#171717] p-2 rounded-full'>{data.prefrence.title2}</span>
                     <span className='bg-[#171717] p-2 rounded-full'>{data.prefrence.title3}</span>
