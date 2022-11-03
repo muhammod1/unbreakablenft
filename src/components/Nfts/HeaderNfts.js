99f9f936a4d8a34b67ca22cdd3886980dd3e8f62
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-11-04 00:09:44 +0100

    [CHORE]-minor-edits-Unbroken-Nfts

--- src/components/Nfts/HeaderNfts.js
+++ src/components/Nfts/HeaderNfts.js
@@ -8,12 +8,12 @@ const HeaderNfts = () => {
       className="mt-[-70px] w-full overflow-hidden stacked"
     >
       <div className="nft-content bg-nftBgImg h:screen md:h-[600px] w-full">
-        <div className="h-3/4 mt-auto mt-[75%] md:mt-[30%]">
-          <h3 className="text-white font-audio text-center text-[52px] sm:text-[63px] leading-[91%]  md:text-[100px] mx-6 md:mx-auto">
+        <div className="h-3/4 md:mt-[10%]">
+          <h3 className="text-white font-audio  text-[52px] sm:text-[63px] leading-[91%]  md:text-[70px] mx-44 ">
             <span className="block">Unbroken NFTs</span>{" "}
           </h3>
-          <p className="mt-[15px] md:w-3/5 w-[92%] mx-auto text-center  text-grey_p">
-            Welcome to our NFT gallery. With your support, we will execute our
+          <p className="mt-[15px] md:w-3/5 w-[92%]  mx-44 text-grey_p">
+            Welcome to our NFT gallery. With your support, we will execute <br/>our
             anti-bullying initiatives.
           </p>
         </div>
