5a2109233c257cf6c4c1ffaa793f08f005d8d22c
muhammod1 <muhammodholawale@gmail.com>
2022-11-04 11:37:13 +0100

    NFTs modal page

--- src/components/Nfts/HeaderNfts.js
+++ src/components/Nfts/HeaderNfts.js
@@ -8,7 +8,7 @@ const HeaderNfts = () => {
       className="mt-[-70px] w-full overflow-hidden stacked"
     >
       <div className="nft-content bg-nftBgImg h-[600px]  small_l:h-[600px] md:h-[600px] w-full">
-        <div className="h-[300px] mobile:h-3/4 mt-[35%] mobile:mt-[25%] md:mt[18%] mx-auto w-[95%] max-w-[1220px] ">
+        <div className="h-[300px] mobile:h-3/4 mt-[45%] mobile:mt-[25%] md:mt[18%] mx-auto w-[95%] max-w-[1220px] ">
           <h3 className="text-white font-audio text-[40px] mobile:text-[50px] sm:text-[63px] leading-[91%]  md:text-[70px] ">
             <span className="block">Unbroken NFTs</span>{" "}
           </h3>
