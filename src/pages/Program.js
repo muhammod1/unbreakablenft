969fa2b6cb9ea744fa7cbf67f4b2420f33a5cd22
kcpele <fidekg123@gmail.com>
2022-11-06 21:03:24 +0100

    remove pagination

--- src/pages/Program.js
+++ src/pages/Program.js
@@ -1,14 +1,14 @@
-import React, { useState} from 'react'
-import ProgramCard from '../components/program/ProgramCard'
-import ProgramHero from '../components/program/ProgramHero'
-import Pagination from '../components/program/paginationPro'
+import React, { useState } from "react";
+import ProgramCard from "../components/program/ProgramCard";
+import ProgramHero from "../components/program/ProgramHero";
+import Pagination from "../components/program/paginationPro";
 
 import { schoolDetails } from "../assets/program";
 
 const Program = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(4);
-  
+
   // Get current posts
 
   const indexOfLastPost = currentPage * postsPerPage;
@@ -16,23 +16,23 @@ const Program = () => {
   const currentPosts = schoolDetails.slice(indexOfFirstPost, indexOfLastPost);
 
   //Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   const paginateFront = () => setCurrentPage(currentPage + 1);
   const paginateBack = () => setCurrentPage(currentPage - 1);
   return (
     <div>
       <ProgramHero />
       <ProgramCard schoolDetails={currentPosts} />
-      <Pagination
+      {/* <Pagination
         postsPerPage={postsPerPage} 
         totalPosts={schoolDetails.length} 
         currentPage={currentPage}
         paginateBack={paginateBack}
         paginateFront={paginateFront}
         paginate={paginate} 
-      />
+      /> */}
     </div>
-  )
-}
+  );
+};
 
-export default Program
+export default Program;
